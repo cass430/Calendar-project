e20480f69c5fbb99de9414b56a085fa589962961
cass430 <156031807+cass430@users.noreply.github.com>
2024-06-11 13:00:38 -0600

    New clock animation
    updated html, css, and javascript

--- assets/calendar.js
+++ assets/calendar.js
@@ -291,16 +291,10 @@ var d = new Date();
 
 //calls function every 1000 miliseconds or every second
 setInterval(function () {
-  d = new Date();
-  second = d.getSeconds() * 6;
-  minute = d.getMinutes() * 6;
-  hour = d.getHours() * 30 + Math.round(minute / 12);
-  document.getElementById("second-hand").style.transform =
-    "rotate(" + second + "deg)";
-
-  document.getElementById("minute-hand").style.transform =
-    "rotate(" + minute + "deg)";
-
-  document.getElementById("hour-hand").style.transform =
-    "rotate(" + hour + "deg)";
+  /* to set current time */
+  const time = new Date();
+  const hour = -3600 * (time.getHours() % 12);
+  const mins = -60 * time.getMinutes();
+  app.style.setProperty("--_dm", `${mins}s`);
+  app.style.setProperty("--_dh", `${hour + mins}s`);
 }, 1000);
